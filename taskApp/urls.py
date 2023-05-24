1bccb8d7fa832073a57a708966af758141bd06fd
Akash <akashshere19@gmail.com>
2023-05-24 18:46:15 +0530

    add pk in urls

--- taskApp/urls.py
+++ taskApp/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     path('tasks/', TaskListView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/',TaskDetailView.as_view(), name='task-retrieve-update-delete'),
+    path('tasks/<int:task_id>/',TaskDetailView.as_view(), name='task-retrieve-update-delete'),
     # path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
     # path('employees/<int:pk>/', EmployeeRetrieveUpdateDeleteView.as_view(), name='employee-retrieve-update-delete'),
     # path('employees/<int:pk>/tasks/', EmployeeTasksListView.as_view(), name='employee-tasks-list'),
